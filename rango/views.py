2cbc21cf1ec5404fb9b381fe792c604b9794be33
vinay singh <aitvinay@gmail.com>
2014-12-12 19:12:30 +0530

    commit

--- rango/views.py
+++ rango/views.py
@@ -61,11 +61,11 @@ def index(request):
         if (datetime.now() - datetime.strptime(last_visit_time[:-7], "%Y-%m-%d %H:%M:%S")).days > 0:
             request.session['visits'] = visits + 1
     else:
-        # The get returns None, and the session does not have a value for the last visit.
+       
         request.session['last_visit'] = str(datetime.now())
         request.session['visits'] = 1
 
-    # Render and return the rendered response back to the user.
+    
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
@@ -74,8 +74,7 @@ def about(request):
     context_dict = {}
     cat_list = get_category_list()
     context_dict['cat_list'] = cat_list
-    # If the visits session varible exists, take it and use it.
-    # If it doesn't, we haven't visited the site so set the count to zero.
+   
 
     count = request.session.get('visits',0)
 
@@ -88,31 +87,26 @@ def category(request, category_name_url):
     # Request our context
     context = RequestContext(request)
 
-    # Change underscores in the category name to spaces.
-    # URL's don't handle spaces well, so we encode them as underscores.
+    
     category_name = decode_url(category_name_url)
 
-    # Build up the dictionary we will use as out template context dictionary.
+    
     context_dict = {'category_name': category_name, 'category_name_url': category_name_url}
 
     cat_list = get_category_list()
     context_dict['cat_list'] = cat_list
 
     try:
-        # Find the category with the given name.
-        # Raises an exception if the category doesn't exist.
-        # We also do a case insensitive match.
+       
         category = Category.objects.get(name__iexact=category_name)
         context_dict['category'] = category
-        # Retrieve all the associated pages.
-        # Note that filter returns >= 1 model instance.
+        
         pages = Page.objects.filter(category=category).order_by('-views')
 
-        # Adds our results list to the template context under name pages.
+        
         context_dict['pages'] = pages
     except Category.DoesNotExist:
-        # We get here if the category does not exist.
-        # Will trigger the template to display the 'no category' message.
+        
         pass
 
     if request.method == 'POST':
@@ -146,14 +140,13 @@ def add_category(request):
             # The user will be shown the homepage.
             return index(request)
         else:
-	        # The supplied form contained errors - just print them to the terminal.
+	        
             print form.errors
     else:
         # If the request was not a POST, display the form to enter details.
         form = CategoryForm()
 
-    # Bad form (or form details), no form supplied...
-    # Render the form with error messages (if any).
+   
     context_dict['form'] = form
     return render_to_response('rango/add_category.html', context_dict, context)
 
@@ -204,28 +197,25 @@ def register(request):
     cat_list = get_category_list()
     context_dict = {}
     context_dict['cat_list'] = cat_list
-    # Boolean telling us whether registration was successful or not.
-    # Initially False; presume it was a failure until proven otherwise!
+    
     registered = False
 
-    # If HTTP POST, we wish to process form data and create an account.
+    
     if request.method == 'POST':
-        # Grab raw form data - making use of both FormModels.
+       
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
-        # Two valid forms?
+        
         if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data. That one is easy.
+            
             user = user_form.save()
 
-            # Now a user account exists, we hash the password with the set_password() method.
-            # Then we can update the account with .save().
+           
             user.set_password(user.password)
             user.save()
 
-            # Now we can sort out the UserProfile instance.
-            # We'll be setting values for the instance ourselves, so commit=False prevents Django from saving the instance automatically.
+            
             profile = profile_form.save(commit=False)
             profile.user = user
 
@@ -236,7 +226,7 @@ def register(request):
             # Now we save the model instance!
             profile.save()
 
-            # We can say registration was successful.
+          
             registered = True
 
         # Invalid form(s) - just print errors to the terminal.
@@ -274,24 +264,23 @@ def user_login(request):
         # A User object is returned if correct - None if not.
         user = authenticate(username=username, password=password)
 
-        # A valid user logged in?
+        
         if user is not None:
-            # Check if the account is active (can be used).
-            # If so, log the user in and redirect them to the homepage.
+           
             if user.is_active:
                 login(request, user)
                 return HttpResponseRedirect('/rango/')
-            # The account is inactive; tell by adding variable to the template context.
+            
             else:
                 context_dict['disabled_account'] = True
                 return render_to_response('rango/login.html', context_dict, context)
-        # Invalid login details supplied!
+      
         else:
             print "Invalid login details: {0}, {1}".format(username, password)
             context_dict['bad_details'] = True
             return render_to_response('rango/login.html', context_dict, context)
 
-    # Not a HTTP POST - most likely a HTTP GET. In this case, we render the login form for the user.
+   
     else:
         return render_to_response('rango/login.html', context_dict, context)
 
@@ -303,10 +292,10 @@ def restricted(request):
     context_dict['cat_list'] = cat_list
     return render_to_response('rango/restricted.html', context_dict, context)
 
-# Only allow logged in users to logout - add the @login_required decorator!
+
 @login_required
 def user_logout(request):
-    # As we can assume the user is logged in, we can just log them out.
+    
     logout(request)
 
     # Take the user back to the homepage.
