542adda4e56ecc2dc888c019b978b05d5fad10bb
vinay singh <aitvinay@gmail.com>
2014-12-12 19:09:09 +0530

    Update urls.py

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -6,15 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^tango_with_django_project/', include('tango_with_django_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    
     
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
